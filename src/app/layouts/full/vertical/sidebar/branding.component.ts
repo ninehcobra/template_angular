3bacbea666c40d9bf4f69a00a9750a2df8a0ffa3
chinhtruong2903 <ttbexinhtt2903@gmail.com>
2024-09-09 15:53:58 +0700

    first commit

---
+++ src/app/layouts/full/vertical/sidebar/branding.component.ts
@@ -0,0 +1,35 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { CoreService } from 'src/app/services/core.service';
+
+@Component({
+  selector: 'app-branding',
+  standalone: true,
+  imports: [NgIf],
+  template: `
+    <div class="branding">
+      @if(options.theme === 'light') {
+      <a href="/">
+        <img
+          src="./assets/images/logos/dark-logo.svg"
+          class="align-middle m-2"
+          alt="logo"
+        />
+      </a>
+      } @if(options.theme === 'dark') {
+      <a href="/">
+        <img
+          src="./assets/images/logos/light-logo.svg"
+          class="align-middle m-2"
+          alt="logo"
+        />
+      </a>
+      }
+    </div>
+  `,
+})
+export class BrandingComponent {
+  options = this.settings.getOptions();
+
+  constructor(private settings: CoreService) {}
+}
